8b040f657358bcb7886e1c740911ec6672dc47dd
Dang Nguyen <dang123vvk@gmail.com>
2021-07-04 22:53:04 +0700

    Update side bar and add sample element

--- src/components/generic/card/index.tsx
+++ src/components/generic/card/index.tsx
@@ -1,10 +1,49 @@
-interface Props {
-    data: Object
-}
-export const Card = ({ data }: Props) => {
+
+import Image from 'next/image'
+import leo from '../../../../public/leo.jpeg'
+import ruby from '../../../../public/diamond.ico';
+import { MdChevronRight } from "react-icons/md";
+// interface Props {
+//     data: Object
+// }
+export const Card = () => {
     return (
         <div className="card">
+            <div className='card-infor-basic'>
+                <div className='card-infor-basic-content'>
+                    <div className='card-infor-basic-content-value'>
+                        <Image src={leo} width={30} height={30} className='avatar' role='button' />
+                    </div>
+                    <div className='card-infor-basic-content-value'>
+                        <Image src={ruby} width={20} height={20} role='button' />
+                    </div>
+                    <div className='card-infor-basic-content-value'>
+                        <span>28 rubies</span>
+                    </div>
+                </div>
+            </div>
+            <div className='card-progress-bar'>
+                <div className='card-progress-bar-content'>
+                    <div className='card-progress-bar-point'>
+                        <span className='card-progress-bar-point-wrap'><b className='card-progress-bar-point-number'>112</b> points</span>
+                    </div>
+                    <div className='card-progress-bar-element'>
+                        <div className='card-progress-bar-element-start'>
 
+                        </div>
+                        <div className='card-progress-bar-element-center'>
+
+                        </div>
+                        <div className='card-progress-bar-element-end'>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div className='card-title'>
+                <span className='card-title-content' role='button'>My progress <MdChevronRight /></span>
+            </div>
         </div>
     )
 }
--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,22 +1,66 @@
-import { MdMenu, MdKeyboardArrowLeft } from "react-icons/md";
+import { MdMenu, MdKeyboardArrowLeft, MdViewQuilt } from "react-icons/md";
+import { Card } from "../generic/card";
 
 export const SideBar = () => {
     return (
         <div className='sidebar-container'>
             <div className='sidebar-menu'>
                 <div className='sidebar-menu-title'>
-                        <span> LABA</span>
+                    <span> LABA</span>
                 </div>
                 <div className='sidebar-menu-icon'>
                     <button className='sidebar-menu-icon-btn'>
-                        <MdKeyboardArrowLeft size={20} color={'#3C3D4E'} />  <MdMenu size={20} color={'#3C3D4E'}/>
+                        <MdKeyboardArrowLeft size={20} color={'#3C3D4E'} />  <MdMenu size={20} color={'#3C3D4E'} />
                     </button>
                 </div>
 
             </div>
-            <div className='sidebar-information'></div>
-            <div className='sidebar-study'></div>
-            <div className='sidebar-support'></div>
+            <div className='sidebar-information'>
+                <Card />
+            </div>
+            <div className='sidebar-study'>
+                <div className='sidebar-study-content'>
+                    <div className='sidebar-study-content-title'>
+                        <div className='sidebar-study-content-title-icon'>
+                            <MdViewQuilt size={20} color={'#3C3D4E'} />
+                        </div>
+                        <div className='sidebar-study-content-title-text'>
+                            <span>MY STUDY</span>
+                        </div>
+                    </div>
+                    <div className='sidebar-study-content-data'>
+                        <div className='sidebar-study-content-data-li'>
+                            <span>All Tasks</span>
+                        </div>
+                        <div className='sidebar-study-content-data-li'>
+                            <span>Knowledge base</span>
+                        </div>
+                        <div className='sidebar-study-content-data-li'>
+                            <span>Rating</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='sidebar-study'>
+                <div className='sidebar-study-content'>
+                    <div className='sidebar-study-content-title'>
+                        <div className='sidebar-study-content-title-icon'>
+                            <MdViewQuilt size={20} color={'#3C3D4E'} />
+                        </div>
+                        <div className='sidebar-study-content-title-text'>
+                            <span>SUPPORT</span>
+                        </div>
+                    </div>
+                    <div className='sidebar-study-content-data'>
+                        <div className='sidebar-study-content-data-li'>
+                            <span>FQA</span>
+                        </div>
+                        <div className='sidebar-study-content-data-li'>
+                            <span>Ask a question</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
